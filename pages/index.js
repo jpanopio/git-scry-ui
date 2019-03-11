0829585dfcfe81495bb4afdbdb3b37dab9079931
Julien <jcpanopio1@gmail.com>
2019-03-11 15:08:07 -0700

    Install isomorphic-unfetch and test it out

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   ScreenClassProvider,
   Container,
   Row,
   Col,
 } from 'react-grid-system';
-import styled from 'styled-components';
-import { COLOR_MAIN_BACKGROUND } from '../constants/colors';
-import Header from '../components/Header';
+import fetch from 'isomorphic-unfetch';
 import App from '../components/App';
 import Card from '../components/Card';
 
-const Index = () => (
-  <App>
-    <Container>
-      <Card>
-        <Row>
-          <Col>
-            <p>Hello next.js</p>
-          </Col>
-          <Col>
-            <p>YEET.js</p>
-          </Col>
-        </Row>
-      </Card>
-    </Container>
-  </App>
-);
+class Index extends Component {
+  static async getInitialProps({ req = {} }) {
+    const response = await fetch('https://google.com');
+
+    console.log('result', response);
+
+    return {};
+  }
+
+  render() {
+    return (
+      <App>
+        <Container>
+          <Card>
+            <Row>
+              <Col>
+                <p>Hello next.js</p>
+              </Col>
+              <Col>
+                <p>YEET.js</p>
+              </Col>
+            </Row>
+          </Card>
+        </Container>
+      </App>
+    );
+  }
+}
 
 export default Index;
