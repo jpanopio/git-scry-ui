073e76f3e7a84ca131dc06b66cb5648043f56e95
Angie Liu <angie.liu@narvar.com>
2019-03-11 17:58:00 -0700

    git scry log in page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import Link from 'next/link'
+import {
+  COLOR_CARD_BACKGROUND,
+  COLOR_CARD_BORDER,
+  COLOR_PRIMARY,
+  COLOR_FONT_PRIMARY,
+  COLOR_MAIN_BACKGROUND
+} from '../constants/colors';
+
 import {
   ScreenClassProvider,
   Container,
   Row,
   Col,
 } from 'react-grid-system';
+
 import styled from 'styled-components';
-import { COLOR_MAIN_BACKGROUND } from '../constants/colors';
 import Header from '../components/Header';
 import App from '../components/App';
 import Card from '../components/Card';
+
+const StyledTitle = styled.h1`
+  font-size: 1.5rem;
+  color: ${COLOR_PRIMARY};
+`;
 
 const Index = () => (
   <App>
@@ -17,12 +31,32 @@ const Index = () => (
       <Card>
         <Row>
           <Col>
-            <p>Hello next.js</p>
-          </Col>
-          <Col>
-            <p>YEET.js</p>
+            <StyledTitle>What is Git Scry?</StyledTitle>
+            <p>During your company's self-reflection and review periods,
+            Git Scry allows you to simply click a few buttons to export
+            your work history during the past cycle in a useful format.
+            Currently, engineers spend at least couple of hours writing their
+            self reflections. Git Scry can save a company these countless
+            engineering hours, encourage descriptive pull request titles and descriptions,
+            and help engineers set sights on future areas for growth and development.</p>
+            <StyledTitle>How to Use?</StyledTitle>
+            <p>Click the button below, log in to github and select the repo and PRs you want to
+            include in your self-reflection. It's that easy!</p>
           </Col>
         </Row>
+        <a href="https://github.com/login">
+          <button style={{ fontSize: 20, color: COLOR_FONT_PRIMARY}}>Go to Github Page</button>
+        </a>
+        <style global jsx>{`
+          button {
+            height:50px;
+            width: 200px;
+            background: white;
+            margin: 15px 2px;
+            border: 3px solid ${COLOR_CARD_BORDER};
+            background-color: ${COLOR_PRIMARY};
+          }
+        `}</style>
       </Card>
     </Container>
   </App>
