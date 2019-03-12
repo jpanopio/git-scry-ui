eef20fcc6f8a3caacc9f6fa44e5e312df2df8640
Julien <jcpanopio1@gmail.com>
2019-03-12 14:12:57 -0700

    Add a Button component, use it in index.js

---
+++ components/Button.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  COLOR_PRIMARY,
+  COLOR_FONT_PRIMARY,
+} from '../constants/colors';
+
+const StyledButton = styled.button`
+  width: ${(props) => props.s.width || '100%'};
+  height: 50px;
+  margin: 15px 0;
+  font-size: 20px;
+  color: ${COLOR_FONT_PRIMARY};
+  background-color: ${COLOR_PRIMARY};
+`;
+
+const Button = ({
+  s = {},
+  children,
+}) => (
+  <StyledButton s={s}>
+    <span>
+      {children}
+    </span>
+  </StyledButton>
+);
+
+export default Button;
