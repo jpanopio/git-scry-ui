17afa1c0d6a2a0b9a53c53f9750cce6bd3c203d6
Julien <jcpanopio1@gmail.com>
2019-03-12 12:53:45 -0700

    Move logos to components, change landing styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
-
+import fetch from 'isomorphic-unfetch';
+import styled from 'styled-components';
+import {
+  ScreenClassProvider,
+  Container,
+  Row,
+  Col,
+} from 'react-grid-system';
+import Header from '../components/Header';
+import App from '../components/App';
+import Card from '../components/Card';
+import CrystalBall from '../components/CrystalBall';
 import {
   COLOR_CARD_BACKGROUND,
   COLOR_CARD_BORDER,
@@ -7,32 +18,15 @@ import {
   COLOR_FONT_PRIMARY,
   COLOR_MAIN_BACKGROUND
 } from '../constants/colors';
-
 import {
-  ScreenClassProvider,
-  Container,
-  Row,
-  Col,
-} from 'react-grid-system';
-
-
-
-import styled from 'styled-components';
-import Header from '../components/Header';
-import App from '../components/App';
-import Card from '../components/Card';
+  GIT_SCRY_CHECK_LOGGED_IN,
+  GIT_SCRY_LOGIN_URL,
+} from '../constants/endpoints';
 
 const StyledTitle = styled.h1`
   font-size: 1.5rem;
   color: ${COLOR_PRIMARY};
 `;
-
-import fetch from 'isomorphic-unfetch';
-
-import {
-  GIT_SCRY_CHECK_LOGGED_IN,
-  GIT_SCRY_LOGIN_URL,
-} from '../constants/endpoints';
 
 class Index extends Component {
   static async getInitialProps(ctx) {
@@ -56,10 +50,10 @@ class Index extends Component {
     return (
       <App>
         <Container>
-          <Card>
+          <Card s={{ margin: '3rem 1rem' }}>
             <Row>
               <Col>
-                <StyledTitle>What is Git Scry?</StyledTitle>
+                <StyledTitle>Why Git Scry?</StyledTitle>
                 <p>During your company's self-reflection and review periods,
                 Git Scry allows you to simply click a few buttons to export
                 your work history during the past cycle in a useful format.
@@ -71,29 +65,22 @@ class Index extends Component {
                 <p>Click the button below, log in to github and select the repo and PRs you want to
                 include in your self-reflection. It's that easy!</p>
                 <a href={GIT_SCRY_LOGIN_URL}>
-                  <button style={{ fontSize: 20, color: COLOR_FONT_PRIMARY}}>Go to Github Page</button>
+                  <button
+                    style={{
+                      width: '100%',
+                      fontSize: 20,
+                      color: COLOR_FONT_PRIMARY,
+                      height: '50px',
+                      margin: '15px 0',
+                      backgroundColor: COLOR_PRIMARY,
+                    }}
+                  >
+                    Log in with GitHub
+                  </button>
                 </a>
-                <style global jsx>{`
-                  button {
-                    height:50px;
-                    width: 200px;
-                    background: white;
-                    margin: 15px 2px;
-                    border: 3px solid ${COLOR_CARD_BORDER};
-                    background-color: ${COLOR_PRIMARY};
-                  }
-                `}</style>
               </Col>
               <Col>
-              <img src="/static/images/logoCrystalBall.png" alt="my image" width="100" height="100"/>
-              <style global jsx>{`
-                img {
-                  height:270px;
-                  width: 270px;
-                  background: white;
-                  margin: 30px 50px 100px 50px;
-                }
-              `}</style>
+                <CrystalBall />
               </Col>
             </Row>
           </Card>
